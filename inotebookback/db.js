4d0e7021f99921a513a9b6d5c40dcbc101bbb4d3
codingwithharoon <haroonlastmail@gmail.com>
2025-06-10 11:14:03 +0500

    changes done in db.js file

--- inotebookback/db.js
+++ inotebookback/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const mongourl= ('mongodb://localhost:27017/wwe');
-//connection
+//connection to mongodb
 const connectToMongo= async ()=>
 {
     await mongoose.connect(mongourl)
